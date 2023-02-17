d9e77688bd47a435053491411a1e42bf94c2e126
Charles Kamiri <charleskamiri6@gmail.com>
2023-02-17 10:43:20 +0300

    first commit

---
+++ src/main/java/com/demo/code_commune/CodeCommuneApplication.java
@@ -0,0 +1,13 @@
+package com.demo.code_commune;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CodeCommuneApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CodeCommuneApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/demo/code_commune/CodeCommuneApplicationTests.java
@@ -0,0 +1,13 @@
+package com.demo.code_commune;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CodeCommuneApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
